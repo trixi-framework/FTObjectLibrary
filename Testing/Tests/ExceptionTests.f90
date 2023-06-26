7ee9733e07d9494b4313f9790fd8e882a430c73f
DavidAKopriva <85404032+DavidAKopriva@users.noreply.github.com>
2023-06-26 08:34:45 -0700

    Update Testing/Tests/ExceptionTests.f90
    Co-authored-by: Andrew Winters <andrew.ross.winters@liu.se>

--- Testing/Tests/ExceptionTests.f90
+++ Testing/Tests/ExceptionTests.f90
@@ -117,8 +117,8 @@
          ALLOCATE(e)
          CALL e % initFatalException(msg = "I'm Sorry, I can't do that Dave  (Designed Uncaught)")
          CALL FTAssertEqual(expectedValue = FT_ERROR_FATAL, &
-                            actualValue    = e % severity(),  &
-                            msg = "Fatal error level match")
+                            actualValue   = e % severity(),  &
+                            msg           = "Fatal error level match")
          CALL throw(e)
          CALL releaseFTException(e)
 
